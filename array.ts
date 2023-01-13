d791fb007628fce856df28b5d5b12c8cdbf310cf
Shaul Kahane <35196468+shaolkahna@users.noreply.github.com>
2023-01-13 14:28:49 +0200

    array + linklist finish

--- array.ts
+++ array.ts
@@ -1,12 +1,71 @@
-let array: Array<number> = [];
+class array_db<T> {
 
+    public array:  Array<number> = [];
 
-for (let index = 0; index < 10000; index++) {
-    let a = Math.floor(Math.random() * 500001)
-      if (array.indexOf(a))
-      {array.push(a)}
+    public search(data: number): boolean{
+      for (let index = 0; index < this.array.length; index++) {
+        if (this.array[index] === data) {
+          return true
+        }
+      }
+      return false
+    }
+    
+    public sortArray() {
+      for (let i = 0; i < this.array.length; i++) {
+          for (let j = 0; j < this.array.length - i - 1; j++) {
+              if (this.array[j] > this.array[j + 1]) {
+                  let temp = this.array[j];
+                  this.array[j] = this.array[j + 1];
+                  this.array[j + 1] = temp;
+              }
+          }
+      }
+      return this.array;
+    }
+    
+    public binarySearch(target: number) {
+        let left = 0;
+        let right = this.array.length - 1;
+        while (left <= right) {
+            let mid = Math.floor((left + right) / 2);
+            if (this.array[mid] === target) {
+                return mid;
+            } else if (this.array[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
 }
 
-for (let index = 0; index <50; index++) {
-  console.log(array[index])
+
+const arrayList = new array_db<Number>();
+
+
+for (let index = 0; arrayList.array.length < 4; index++) {
+    let a = Math.floor(Math.random() * 4) + 1;
+    if (!arrayList.search(a))
+    {
+      arrayList.array.push(a)
+    }
 }
+
+for (let index = 0; index <arrayList.array.length; index++) {
+  console.log(arrayList.array[index])
+}
+
+console.log(arrayList.search(2))
+console.log(arrayList.search(5))
+
+
+arrayList.sortArray()
+
+for (let index = 0; index <arrayList.array.length; index++) {
+  console.log(arrayList.array[index])
+}
+
+console.log(arrayList.search(2))
+console.log(arrayList.search(5))
--- linkList.ts
+++ linkList.ts
@@ -1,13 +1,12 @@
-
 let globalindex: number = 0
 
 class LinkedNode<T> {
-    public data: T | null = null;
-    public index: Number | null = 0;
+    public data: number ;
+    public index: number;
     public next: LinkedNode<T> | null = null;
     public prev: LinkedNode<T> | null = null;
 
-    constructor (data: T) {
+    constructor (data: number) {
       this.data = data 
       this.index = globalindex 
       globalindex++                
@@ -21,10 +20,14 @@ class LinkedList<T> {
 
     public getLastNode(this: LinkedList<T>, node: LinkedNode<T>): LinkedNode<T>{
 
-        return node.next ? this.getLastNode(node.next) : node;
+        while (node.next != null) {
+          node = node.next;
+        }
+
+        return node;
     }
 
-    public insertAtEnd(data: T): LinkedNode<T> {
+    public insertAtEnd(data: number): LinkedNode<T> {
         const node = new LinkedNode(data);
         if (!this.head) {
           this.head = node;
@@ -36,7 +39,7 @@ class LinkedList<T> {
         return node;
       }
   
-    public insertInBegin(data: T): LinkedNode<T> {
+    public insertInBegin(data: number): LinkedNode<T> {
 
         const node = new LinkedNode(data);
 
@@ -51,35 +54,159 @@ class LinkedList<T> {
         return node;
     }
 
-    public search(data: number): LinkedNode<T> | null {
-        const checkNext = (node: LinkedNode<T>): LinkedNode<T> | null => {
-          if (data===node.data){
-            return node;
-          }
-          return node.next ? checkNext(node.next) : null;
-        };
-    
-        return this.head ? checkNext(this.head) : null;
+    public search(data: number): Number 
+    {
+      let current = this.head ? this.head : null;
+        while (current != null) {
+            if (current.data === data && current.index != null)
+                return current.index;
+            current = current.next;
+        }
+        return -1;
+    }
+
+    public print(node: LinkedNode<T>): void
+    {
+      console.log(node);
+      while (node.next != null) {
+        node = node.next;
+        console.log(node);
       }
-    
+    }
+
+    public Sort(): void  
+    {
+
+      if (linkedList.head) {
+        let last: LinkedNode<Number> | null = linkedList.getLastNode(linkedList.head)
+        while (last) {
+            if(this.head){
+              let node: LinkedNode<Number> | null = this.head;
+              while (node != last && node ) {
+                let next: LinkedNode<Number> | null = node.next
+                if (next && node.data > next.data) {
+                    let temp = node.data
+                    node.data = next.data 
+                    next.data =  temp;
+                }
+                node = next;
+              }
+          last = last.prev; // shorten the range that must be bubbled through
+        }
+       }
+      } 
+    }
+
+    public insert(data: number): void 
+    {
+
+      let node:  LinkedNode<T> | null = new LinkedNode(data) ;
+
+        if (this.head===null) {
+            this.head = node;
+        } else {
+          let current = this.head ? this.head : null;
+          while (current !== null && current.next !== null && current.data < data) {
+                current = current.next;
+          }
+
+          if (current === this.head) {
+            node.index = this.head.index 
+            this.head.prev = node;
+            node.next = this.head;
+            this.head = node;
+            node = node.next;
+            while (node !== null) {
+              node.index = node.index +1;
+              node = node.next;
+            }
+            return
+          }
+
+          if (current !== null && current.next !== null) {
+            current = current.prev
+          }
+
+          if(node !==null && current !== null){
+            node.index = current.index +1
+            node.next = current.next;
+            current.next = node;
+            node.prev = current
+            if (node.next!= null) {
+              node.next.prev = node
+            }
+            node = node.next;
+            while (node !== null) {
+              node.index = node.index +1;
+              node = node.next;
+            }
+              
+          }
+        }
+    }
+
+
+
 }
 
 const linkedList = new LinkedList<Number>();
 
-for (let index = 0; index < 10000; index++) {
-let a = Math.floor(Math.random() * 500001)
-  linkedList.insertInBegin(a);
+for (let index = 0; globalindex < 4; index++) {
+  let a = Math.floor(Math.random() * 4) +1;
+  if (linkedList.search(a) ===-1) {
+    linkedList.insertAtEnd(a);
+  }
 }
 
-for (let index = 0; index < 500; index++) {
-  let a = linkedList.search(index)
-  if (a)
-    console.log(a?.index, "  ", a?.data)
-  
-  
-}
+// if (linkedList.head) {
+//   linkedList.print(linkedList.head)
+// } 
 
 
+linkedList.Sort();
+
+// if (linkedList.head) {
+//   linkedList.print(linkedList.head)
+// } 
+
+console.log("");
+console.log("");
+console.log("");
+console.log("");
+console.log("");
+console.log("");
+console.log("");
+
+linkedList.insert(5)
+
+if (linkedList.head) {
+  linkedList.print(linkedList.head)
+} 
+
+// for (let index = 0; index < 50000; index++) {
+//   let a: Number = -1
+//   a = linkedList.search(index)
+//   if (a !==-1 && index < 10) {
+//     console.log(index + "  " + a)
+//   }  
+// }
+
+
+// for (let index = 0; index < 50000; index++) {
+//   let a: Number = -1
+//   a = linkedList.search(index)
+//   if (a !==-1 && index < 100) {
+//     console.log(index + "  " + a)
+//   }  
+// }
+
+
+
+
+// if (linkedList.head) {
+//   let u = linkedList.getLastNode(linkedList.head)
+//   console.log(u)
+// } 
 
 
 
